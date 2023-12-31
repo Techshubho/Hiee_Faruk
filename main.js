38ada51cc3f567d421b6bc6027a3137c49e0b90f
Suvam Sarkar <86678413+Techshubho@users.noreply.github.com>
2023-12-31 17:33:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("Wait and see all animation \n Click to start the song")
+
+ window.addEventListener('click', function () {
+    
+    var audio = document.getElementById("Play");
+    audio.play();
+    
+});
